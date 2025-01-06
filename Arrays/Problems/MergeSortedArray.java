d6257053aef52f3d64d041e8b3fc9d9a37d71348
nandhujayakumar <nandhujayakumar21@gmail.com>
2025-01-06 22:52:05 +0530

    Merge Sorted Array problem - bruteforce method - returned runtuimke 1ms - beats 29.21%

--- Arrays/Problems/MergeSortedArray.java
+++ Arrays/Problems/MergeSortedArray.java
@@ -1,9 +1,22 @@
 package Problems;
 
+import java.util.Arrays;
+
 public class MergeSortedArray {
     
     public static void main(String[] args) {
         
-        
+        // [2,3,4,0,0,0]   [5,6,7]
+        // n = 3 m =3
+
+        int num1[] = {1};
+        int num2[] = {};
+        int n = 1;
+
+        for (int i = n, j=0; i < num1.length; i++, j++) {
+                num1[i] = num2[j];
+            }
+            Arrays.sort(num1);
+            System.out.println(Arrays.toString(num1));
     }
 }
