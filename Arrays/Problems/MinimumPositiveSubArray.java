f073dc3c2423d5309e2a9908d7ba8c040e04d214
nandhujayakumar <nandhujayakumar21@gmail.com>
2025-01-18 23:06:38 +0530

    trial

---
+++ Arrays/Problems/MinimumPositiveSubArray.java
@@ -0,0 +1,23 @@
+package Problems;
+
+
+//3364
+
+public class MinimumPositiveSubArray {
+
+    public static void main(String[] args) {
+        
+        int a[] = {3, -2, 1, 4, 10};
+        int l = 2;
+        int r = 4;
+
+        int i=0, j=0;
+
+        while (i < a.length) {
+            
+        }
+
+
+    }
+    
+}
